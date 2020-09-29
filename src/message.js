f5d1584cd9c77d38c33ac799d2414dba16d48b74
Alanna Bouloy <lanihope@gmail.com>
2020-09-29 16:16:56 -0400

    added message.js

---
+++ src/message.js
@@ -0,0 +1,77 @@
+/* eslint-disable default-case */
+import React from 'react'
+
+export default function Message(props){
+    const timeStamp = new Date.getTime(props.timestamp);
+    switch(props.type){
+        case 'message':
+            return <div className='message'>
+            <div>
+                <img src = {props.avatar} alt=''/>
+                <h3>{props.name}</h3>
+                <p className="timestamp">{timeStamp}</p>
+            </div>
+            <div>
+                <p>{props.message}</p>
+            </div>
+         </div>;
+      
+        
+        case 'thumbs-up':
+            return <div class='event'>
+            <h3>{props.name}</h3>
+            <p>gave a thumbs up!</p>
+            <p className="timestamp">{timeStamp}</p>
+            </div>;
+
+        case 'thumbs-down':
+            return <div class='event'>
+            <h3>{props.name}</h3>
+            <p>gave a thumbs down!</p>
+            <p className="timestamp">{timeStamp}</p>
+            </div>;
+
+        case 'raise-hand':
+            return <div class='event'>
+            <h3>{props.name}</h3>
+            <p>raised their hand!</p>
+            <p className="timestamp">{timeStamp}</p>
+            </div>;
+        case 'clap':
+            return <div class='event'>
+            <h3>{props.name}</h3>
+            <p>clapped!</p>
+            <p className="timestamp">{timeStamp}</p>
+            </div>;
+
+        case 'join':
+            return <div class='event'>
+            <h3>{props.name}</h3>
+            <p>joined this session!</p>
+            <p className="timestamp">{timeStamp}</p>
+            </div>;
+
+        case 'leave':
+            return <div class='event'>
+            <h3>{props.name}</h3>
+            <p>left this session!</p>
+            <p className="timestamp">{timeStamp}</p>
+            </div>;
+
+        case 'join-stage':
+            return <div class='event'>
+            <h3>{props.name}</h3>
+            <p>joined the stage!</p>
+            <p className="timestamp">{timeStamp}</p>
+            </div>;
+
+        case 'leave-stage':
+            return <div class='event'>
+            <h3>{props.name}</h3>
+            <p>left the stage!</p>
+            <p className="timestamp">{timeStamp}</p>
+            </div>;
+
+    }
+   
+}
