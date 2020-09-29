bdb3f9379c6be9293cd2aa83ae04a4583553fa74
Alanna Bouloy <lanihope@gmail.com>
2020-09-29 14:41:11 -0400

    added participant.js

---
+++ src/participant.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//takes name, avatar, and inSession
+export default function Participant(props){
+
+    let message = <p className='left-session'/>
+    
+    if(props.inSession && props.onStage){
+        message = <p className= 'in-session'>On stage</p>
+    }else if(props.inSession){
+         message = <p className='in-session'/>
+    }
+
+    
+    return (
+        <div className = 'participant'>
+            <img src={props.avatar} alt=''/>
+            <h3>{props.name}</h3>
+            {message};
+
+        </div>
+    );
+}
